2c6014596dfdf6e0385f19925eb9597c3261f642
Rahul-Khera-Codes <rahul.excel2011@gmail.com>
2024-12-12 02:25:15 +0100

    made meaningful changes to improve the project

---
+++ src/components/ItemGroup.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-for="i in item.items.all()">
+        <ItemGroup v-if="isItemGroup(i)" :item="i" :filter="filter" />
+        <Request v-else :item="i" :filter="filter" />
+    </div>
+</template>
+  
+<script setup>
+import Request from '@/components/Request.vue'
+
+import { isItemGroup } from '../mixins/itemMixin'
+
+const props = defineProps(['item', 'filter']);
+</script>
+  
